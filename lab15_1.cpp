f8c0707d7d4e5ea84f63f2adb59f1cf6974dada3
Autoz <kuro123520@hotmail.com>
2021-02-05 16:37:20 +0700

    lab15_1 - success

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -32,3 +32,36 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a,int N,int M){
+	cout << fixed << setprecision(2);
+		for(int i = 0; i < N*M; i++){
+			cout << *(a+i);
+			if((i+1)%M == 0) cout << endl;
+			else cout << " ";
+		}
+}
+
+void randData(double *a,int N,int M){
+	for(int i = 0; i < N*M ; i++){
+		*(a+i) = (rand()%101)/100.0;
+	}
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+	for(int i = 0; i < N ; i++){
+        *(b+i) = 0;
+		for(int j = 0; j < M ; j++){
+			*(b+i) += *((a+(M*i))+j);
+		}
+	}
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+    for(int i = 0; i < M ; i++){
+        *(b+i) = 0;
+        for(int j = 0; j < N ; j++){
+            *(b+i) += *(a+i+(M*j));          
+        }
+    }
+}
